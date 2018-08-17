85c4c52fabf9a0832ff769cca4fbae3d0709cf8f
Tom Nunez <tomjnunez@gmail.com>
2018-08-16 22:59:16 -0600

    late to the git party sending commits now

---
+++ lib/event.rb
@@ -0,0 +1,41 @@
+class Event
+  attr_reader :name, :ages
+
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+  end
+
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+
+  def average_age
+    average = ages.sum / ages.length.to_f
+  end
+
+  def subtract_average
+    ages.map do |age|
+      sub_average = age - average_age
+      sub_average.round(1)
+    end
+  end
+
+  def squared
+    subtract_average.map do |number|
+      squared_number = number ** 2
+      squared_number.round(2)
+    end
+  end
+
+  def standard_deviation_age
+
+    ages.map do |age|
+      age - average_age
+    end
+  end
+end
--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,43 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+sum_of_ages = ages.sum.to_f
 
-# When you find the standard deviation, print it out
+number_of_elements = ages.length
+
+mean = sum_of_ages / ages.length
+
+subtraction_array = []
+
+ages.map do |age|
+  age - mean
+  subtraction_array << age - mean
+end
+
+float_array = []
+
+subtraction_array.map do |digit|
+  digit.round(1)
+float_array << digit.round(1)
+end
+
+squared_array = []
+
+float_array.map do |digit|
+  digit**2
+squared_array << digit**2
+end
+
+squared_float_array = []
+
+squared_array.map do |digit|
+  digit.round(2)
+squared_float_array << digit.round(2)
+end
+
+squared_float_array.sum
+
+step_7 = squared_float_array.sum / number_of_elements
+
+square_root = Math.sqrt(step_7)
+
+p square_root.round(2)
